587f76cb66ee45b9984c687dd8198695ad9e0793
Yuktigenesis <bod.yukti@gmail.com>
2026-01-11 15:03:59 +0530

    Insights versal code added

--- Yukti-APP/src/main.tsx
+++ Yukti-APP/src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import AppToaster from "./components/AppToaster.tsx";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <SpeedInsights/>
     <App />
     <AppToaster />
   </React.StrictMode>,
